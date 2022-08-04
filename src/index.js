c25baaea3cc701527a112be64c9c25d7487cb445
sewonjun <93499071+sewonjun@users.noreply.github.com>
2022-08-04 17:52:35 +0900

    state 끌어올리기

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { toHaveStyle } from "@testing-library/jest-dom/dist/matchers";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
@@ -5,12 +6,7 @@ import "./index.css";
 class Square extends React.Component {
   render() {
     return (
-      <button
-        className="square"
-        onClick={() => {
-          console.log("click");
-        }}
-      >
+      <button className="square" onClick={() => this.props.onClick()}>
         {this.props.value}
       </button>
     );
@@ -18,8 +14,24 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({ squares: squares });
+  }
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
